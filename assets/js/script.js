d5111ad565adf278e95578dec7ebfaeb87abedf6
Stephen Fudge <sfudge@gmail.com>
2022-05-26 21:17:56 -0400

    added starting point on files

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// shows current date in day, month, number
+var whatDay = moment().format("dddd, MMMM Do");
+$("#currentDay").text(whatDay);
+
+var whatTime = moment().format()
+
+
+
+// if statements for time, if current time then one color, 
+// if past another if future another color
+// css classes are .hour .past .present .future
+
+
+//moment().hours()
+
+
+// save button = local storage
+
+// id for each time
+
