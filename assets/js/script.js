4678aaad0fd271bc10d4120080d74ce8af9d1ecd
Stephen Fudge <sfudge@gmail.com>
2022-05-27 16:35:31 -0400

    js functions for time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,34 @@
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
-var currentTime = moment().hours()
+// makes current time, the current time based on moment
+var currentTime = moment().hour()
 
 
+// function to get value of each time block then output the saved local store entry
+$(".time-block").each(function () {
+    var hour = $(this).attr("id").split("-")[1];
 
-// if statements for time, if current time then one color, 
-// if past another if future another color
-// css classes are .hour .past .present .future
+    var userInput = localStorage.getItem(hour);
+    var textArea = $(this).find(".description");
+    textArea.val(userInput);
 
 
-//moment().hours()
+    //confirms if timeblocks are before/after/in the current hour
+    if (hour < currentTime) {
+        $(this).find(".description").addClass("past");
+    } else if (hour == currentTime) {
+        $(this).find(".description").addClass("present");
+    } else {
+        $(this).find(".description").addClass("future");
+    }
+});
 
-
-// save button = local storage
-
-// id for each time
+// save button function
+$(".saveBtn").on("click", function () {
+    //which time block to save it to 
+    var block = $(this).parent().attr("id").split("-")[1];
+    //what to save
+    var textEntry = $(this).parent().find(".description").val();
+    localStorage.setItem(block, textEntry);
+});
